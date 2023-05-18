a2394b8731cd6ab7a79c5e682898139328b29e23
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-05-18 23:43:31 +0200

    V1.6

--- src/main/java/com/example/air_hockey_with_java/Menu.java
+++ src/main/java/com/example/air_hockey_with_java/Menu.java
@@ -81,8 +81,6 @@ public class Menu extends GridPane {
 
         //Create a Rectangle to add button on it
         Rectangle rectangle = new Rectangle(recWidth, recHeight);
-        rectangle.setLayoutX((double) Width / 2 - recWidth / 2);
-        rectangle.setLayoutY((double) Height / 2 - recHeight / 2);
         rectangle.setFill(Color.rgb(200, 34, 255, .2));
         rectangle.setStroke(Color.rgb(200, 34, 255, .8));
         rectangle.setStrokeWidth(5);
